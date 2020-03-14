2ab20cbc093d734e868fef8a05f6b8acd8a2fb91
darryl <darryl.s.lam@gmail.com>
2020-03-14 00:26:39 -0700

    Generated more data, added random forests

--- matlab_src/heatsim_parallel.m
+++ matlab_src/heatsim_parallel.m
@@ -3,66 +3,85 @@ clc;
 
 tic
 
-init_temp_range = 15:5:25;
-final_temp_range = 80:5:100;
-rise_temp_range = 10:5:40;
+initTempRange = 15:5:25;
+finalTempRange = 80:5:100;
+riseTempRange = 10:5:40;
+specificHeatRange = [921];
+thermalConductivityRange = [150];
+massDensityRange = [2.70*1000];
+materialRange = ["Al"];
 
-iteration_array = cell(length(init_temp_range), length(final_temp_range), length(rise_temp_range));
+iteration_array = cell(length(initTempRange), length(finalTempRange), length(riseTempRange), length(specificHeatRange), length(thermalConductivityRange), length(massDensityRange), length(materialRange));
 iterations = size(iteration_array);
 
-thermalmodelT = createpde('thermal','transient');
+delete(gcp('nocreate'))
+parpool('local',12);
 
-%define the shape of the 'air'
-r1 = [-1.55 1.55 1.55 -1.55 -1.55 -1.55 1.55 1.55];
-r1 = [3 4 r1./100]';
-%define the shape of the 2d sqaure representing a cylinder
-r2 = [-1.5 1.5 1.5 -1.5  -1.5 -1.5 1.5 1.5];
-r2 = [3 4 r2./100]';
-gm = [r1,r2];
-sf = 'r1+r2'; %add the shapes together
-ns = char('r1','r2');
-ns = ns';
-[dl,bt] = decsg(gm,sf,ns);
-geometryFromEdges(thermalmodelT,dl); %thermal model is now a
-
-parfor (i = 1:prod(iterations))
+for rad = 1.80:0.05:1.85   
     
-    [init_temp_iter, final_temp_iter, rise_temp_iter] = ind2sub(iterations,i);
-    inittemp = init_temp_range(init_temp_iter);
-    risetemp = rise_temp_range(rise_temp_iter);
-    finaltemp = final_temp_range(final_temp_iter);
+    thermalmodelT = createpde('thermal','transient');
     
-    thermalProperties(thermalmodelT,'Face',1,'ThermalConductivity',25.72/1000,... %W/m K
-        'MassDensity',1.2041 ,... %kg/m^3
-        'SpecificHeat', 0.718/1000); %J/(kmol*K)
-    thermalProperties(thermalmodelT,'Face',2,'ThermalConductivity',150,... %W/m K
-        'MassDensity',2.70*1000,... %kg/m^3
-        'SpecificHeat',921); %J/(kg k)
-    transientBCHeatedBlockParallel(thermalmodelT, 'Edge', [2,1,7,6], 'Temperature', inittemp, risetemp, finaltemp)
-    msh= generateMesh(thermalmodelT,'Hmax',0.001);
-    %             figure
-    %             pdeplot(thermalmodelT);
-    %             axis equal
-    %             title 'Block With Finite Element Mesh Displayed'
+    air = rad + 0.05;
+    %define the shape of the 'air'
+    r1 = [-air air air -air -air -air air air];
+    r1 = [3 4 r1./100]';
+    %define the shape of the 2d sqaure representing a cylinder
+    r2 = [-rad rad rad -rad  -rad -rad rad rad];
+    r2 = [3 4 r2./100]';
+    gm = [r1,r2];
+    sf = 'r1+r2'; %add the shapes together
+    ns = char('r1','r2');
+    ns = ns';
+    [dl,bt] = decsg(gm,sf,ns);
+    geometryFromEdges(thermalmodelT,dl); %thermal model is now a
     
-    tlist = 0:1:(60*60);
-    thermalIC(thermalmodelT,inittemp);
-    R = solve(thermalmodelT,tlist);
-    T = R.Temperature;
-    
-    getClosestNode = @(p,x,y) min((p(1,:) - x).^2 + (p(2,:) - y).^2);
-    
-    [~,nid] = getClosestNode( msh.Nodes, 0, 0 );
-    [~,nid2] = getClosestNode( msh.Nodes, 1.5,1.5);
-    
-    centerT = T(nid,:);
-    outT = T(nid2,:);
-    csvName = sprintf("temps%d%d%d.csv", inittemp,risetemp,finaltemp)
-    csvMat = [tlist', outT', centerT'];
-    writematrix(csvMat,csvName);
-    
-%     save(Namecenter,'centerT')
-%     save(Nameouter, 'outT')
+    parfor (i = 1:prod(iterations))
+        [initTempIter, finalTempIter, riseTempIter, specHeatIter, thermCondIter, massDensIter, matIter] = ind2sub(iterations,i);
+        
+        inittemp = initTempRange(initTempIter);
+        risetemp = riseTempRange(riseTempIter);
+        finaltemp = finalTempRange(finalTempIter);
+        thermalConductivity = thermalConductivityRange(thermCondIter);
+        massDensity = massDensityRange(massDensIter);
+        specificHeat = specificHeatRange(specHeatIter);
+        material = materialRange(matIter);
+        
+        thermalProperties(thermalmodelT,'Face',1,'ThermalConductivity',25.72/1000,... %W/m K
+            'MassDensity',1.2041 ,... %kg/m^3
+            'SpecificHeat', 0.718/1000); %J/(kmol*K)
+        thermalProperties(thermalmodelT,'Face',2,'ThermalConductivity',thermalConductivity,... %W/m K
+            'MassDensity',massDensity,... %kg/m^3
+            'SpecificHeat',specificHeat); %J/(kg k)
+        transientBCHeatedBlockParallel(thermalmodelT, 'Edge', [2,1,7,6], 'Temperature', inittemp, risetemp, finaltemp)
+        msh= generateMesh(thermalmodelT,'Hmax',0.001);
+        %             figure
+        %             pdeplot(thermalmodelT);
+        %             axis equal
+        %             title 'Block With Finite Element Mesh Displayed'
+        
+        tlist = 0:1:(60*60);
+        thermalIC(thermalmodelT,inittemp);
+        R = solve(thermalmodelT,tlist);
+        T = R.Temperature;
+        
+        getClosestNode = @(p,x,y) min((p(1,:) - x).^2 + (p(2,:) - y).^2);
+        
+        [~,nid] = getClosestNode( msh.Nodes, 0, 0 );
+        [~,nid2] = getClosestNode( msh.Nodes, 1.5,1.5);
+        
+        centerT = T(nid,:);
+        outT = T(nid2,:);
+        specificHeatCSV = zeros(length(tlist),1) + specificHeat;
+        thermalConductivityCSV = zeros(length(tlist),1) + thermalConductivity;
+        massDensityCSV = zeros(length(tlist),1) + massDensity;
+        radCSV = zeros(length(tlist),1) + rad;
+        
+        csvName = sprintf("%.2f%sTemps%d%d%d.csv", rad, material, inittemp,risetemp,finaltemp)
+        csvMat = [tlist', outT', centerT', radCSV, specificHeatCSV, thermalConductivityCSV, massDensityCSV];
+        writematrix(csvMat,csvName);
+        
+        %     save(Namecenter,'centerT')
+        %     save(Nameouter, 'outT')
+    end
 end
-
 toc
